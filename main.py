7960bbf99caeec86ab2e64b638bf50481da7fbf3
mzmarcon <marcon.zm@gmail.com>
2019-11-01 19:47:04 -0300

    update

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import sys
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Device:',device)
 
-params = { 'batch_size': 4,
-           'learning_rate': 1e-4,
+params = { 'batch_size': 40,
+           'learning_rate': 1e-5,
            'weight_decay': 6e-2,
-           'epochs': 5 }
+           'epochs': 50 }
 
 
 training_set = ACERTA_data(set='training', split=0.8)
@@ -37,30 +37,22 @@ optimizer = torch.optim.Adam(model.parameters(), lr=params['learning_rate'],
 for e in range(params['epochs']):
     print('Epoch:',e)
     model.train()
-    total_loss = 0
     iterations = 0
     losses = []
     for i, data in enumerate(train_loader):
-        input_data = Variable(data['input']).to(device)
-        input_data = input_data.unsqueeze(1)
-        # print('Input:',input_data.shape)
+        input_data = Variable(data['input']).float().to(device)
         output = model(input_data)
+
         label = Variable(data['label']).float().to(device)
 
-        # print('Label:',label)
-        # print('Output:',output)
         # print('Output:',output.shape)
 
         loss = criterion(output, label.unsqueeze(1))
         losses.append(loss.item())
-
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        iterations += 1          
-
-        # if iterations % 5 == 0:
-            # print('Training Loss:', np.mean(losses[-5]))
+        iterations += 1
 
         print('Training Loss: {:.3f}'.format(loss.item()))
 
@@ -71,29 +63,29 @@ for e in range(params['epochs']):
     iterations = 0
     for i, data in enumerate(val_loader):
         input_data = Variable(data['input']).to(device)
-        input_data = input_data.unsqueeze(1)
-        # print('Input:',input_data.shape)
         output = model(input_data)
+
         label = Variable(data['label']).float().to(device)
 
         loss = criterion(output, label.unsqueeze(1))
         val_loss.append(loss.item())
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        iterations += 1          
+        iterations += 1 
 
         prediction = (output > 0).float()
         correct = (prediction == label).float().sum()
         accuracy.append(correct / label.shape[0])
 
-        print('Output:',output[0])
 
-        print('Label:',label)
-        print('Prediction:',prediction)
-        print('Acc:',accuracy[-1])
-        # if iterations % 5 == 0:
-        #     print('Accuracy:', np.mean(accuracy))
+        # print('Output:',output[0])
+        # print('Label:',label.shape)
+        # print('Prediction:',prediction)
+        # print("ACC",accuracy)
+        if iterations % 50 == 0:
+            # print('Label',label)
+            # print('Prediction',prediction)
+            # print('Acc:',accuracy[-1])
+            print('Validation Loss: {:.3f}'.format(loss.item()))
 
-    print('Validation Loss: {:.3f}'.format(np.mean(val_loss)))
+    print('Accuracy:', torch.mean(torch.stack(accuracy)))
+
